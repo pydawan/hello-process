d19a2416a7b4401a48686bfdea240dd46cfdcea3
Thiago Monteiro <pydawan@gmail.com>
2017-09-13 08:56:31 -0300

    Adicionados comentários ao código-fonte.

--- src/main/java/br/org/hello/process/ProcessExecutor.java
+++ src/main/java/br/org/hello/process/ProcessExecutor.java
@@ -11,12 +11,20 @@ import java.io.InputStreamReader;
  */
 public class ProcessExecutor {
    public static void main(String args[]) throws IOException, InterruptedException {
+      // Referência o objeto runtime que permite a aplicação se comunicar 
+      // com seu ambiente de execução (Sistema Operacional).
       Runtime rt = Runtime.getRuntime();
+      // Executa o comando especificado com seus argumentos em um processo separado (sub-processo)
+      // do processo principal (main) da aplicação.
       Process proc = rt.exec(args);
+      // Lê o fluxo de entrada do processo Java (processo da JVM) que está conectado
+      // ao processo do runtime (processo do Sistema Operacional).
       BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+      // Mostra cada linha na saída padrão (STDOUT - Standard Output).
       while (br.ready()) {
          System.out.println(br.readLine());
       }
+      // Espera até o sub-processo finalizar sua execução para continuar a execução do processo principal (main).
       proc.waitFor();
    }
 }
